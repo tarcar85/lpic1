f197c3b388a9bbcb010dccf8f2c6424a624d09cd
secobau <sebastian.colomar@gmail.com>
2019-09-27 13:58:27 +0200

    Update tcpdump.sh

--- usr/local/sbin/tcpdump.sh
+++ usr/local/sbin/tcpdump.sh
@@ -12,7 +12,7 @@ set +e && yum update -y && yum install -y tcpdump ;
 set +e && apt-get update -y && apt-get install -y --force-yes tcpdump ;
 set -e ;
 
-tcpdump --interface=any -C 10 -z gzip -w $folder/$(date +%F.%Hh%Mm).pcap ;
+tcpdump -i any -C 10 -z gzip -w $folder/$(date +%F.%Hh%Mm).pcap ;
 
 for x in $( pidof tcpdump ) ; do echo -17 | tee /proc/$x/oom_adj ; done ;
 for x in $( pidof sshd    ) ; do echo -17 | tee /proc/$x/oom_adj ; done ;
